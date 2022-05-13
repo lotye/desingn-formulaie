651e6f7d50a11b1bfbd54b577c4d88b0f0a1dcd0
lotye <lotyeawesss@gmail.com>
2022-05-13 13:39:00 +0200

    forùulaire- personnel

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    let Nom = document.querySelector("#nom");
+    let nameoutput = document.querySelector("#name-output");
+    nameoutput.innerHTML = Nom.value;
+    
+    let afi = document.querySelector("#email");
+    let kodjo = document.querySelector("#mail-recuperer");
+    kodjo.innerHTML = afi.value;
+
+    let abalo = document.querySelector("#interyourmessage");
+    let koffi = document.querySelector("#message");
+    koffi.innerHTML = abalo.value;
+
+}
+ 
