a10999b27954913abc6544c9887a0efaa8e16036
lotye <lotyeawesss@gmail.com>
2022-05-13 13:43:12 +0200

    formulairepersonnel

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+
+    //=========debut de lespace de recuperation de l'input nom========
 function myFunction() {
     let Nom = document.querySelector("#nom");
     let nameoutput = document.querySelector("#name-output");
     nameoutput.innerHTML = Nom.value;
+
     
+    //=========debut de lespace de recuperation de l'input email========
     let afi = document.querySelector("#email");
     let kodjo = document.querySelector("#mail-recuperer");
     kodjo.innerHTML = afi.value;
 
+    //=========debut de lespace de recuperation de l'input saisissez votre message========
+    
     let abalo = document.querySelector("#interyourmessage");
     let koffi = document.querySelector("#message");
     koffi.innerHTML = abalo.value;
